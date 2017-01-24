6d3780b99f1aeebc703658c533f814c82fd9560e
Mark van Kleef <mkleef@avans.nl>
2017-01-24 20:22:24 +0100

    Testing

---
+++ test/game.spec.js
@@ -0,0 +1,33 @@
+describe('game', function(){
+    var gameFactory;
+    var scope;
+    var gameCtrl;
+    var httpBackend;
+
+
+    beforeEach(function(){
+        module('mahjong');
+
+        inject(function($rootScope, $controller, $injector){
+
+            httpBackend = $injector.get('$httpBackend');
+            scope = $rootScope.$new();
+            gameFactory = $injector.get('GameFactory');
+            gameCtrl = $controller('GameController',{$scope: scope});
+
+        })
+    });
+
+    describe('selecting a tile', function(){
+        it('should return false if the selected tile is not available (tiles left and right)', function(){
+            
+            expect(gameCtrl.selectTile(tiles[1])).to.equal(false);
+        });
+
+        it('should return false if the selected tile is not available (tile on top)', function(){
+
+            //Assert
+            expect(true);
+        });
+    });
+});
