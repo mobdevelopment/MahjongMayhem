af5ff0241d1503cdce2a28e2a5c1f099bf1a84fd
Michael Schouten <manschouten@hotmail.com>
2017-02-27 23:40:34 +0100

    added button spectate, no functionality for it

--- app/js/game/controllers/gameController.js
+++ app/js/game/controllers/gameController.js
@@ -74,6 +74,23 @@ module.exports = function($scope, $http, $q, $timeout, gameService, gameFactory,
 		return game.createdBy._id == self.currentUser.username && game.players.length >= game.minPlayers && game.state == "open";
 	};
 
+	self.canSpectate = function(game) {
+		if (self.currentUser.username != undefined && game.state == 'playing') {
+			var count = 0;
+			for (var i = 0; i < game.players.length; i++) {
+				if (game.players[i]._id == self.currentUser.username) {
+					count++;
+				}
+			}
+			if (count == 0) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		return false;
+	}
+
 	self.joinGame = function(gameId) {
 		gameService.joinGame(gameId)
 			.then(function successCallback(response) {
@@ -118,62 +135,7 @@ module.exports = function($scope, $http, $q, $timeout, gameService, gameFactory,
 			self.errorMessage = '';
 		}, 3000);
 	};
-	// self.open = function(size) {
-	// 	var modalInstance = $modal.open({
-	// 		templateUrl: '',
-	// 		controller: '',
-	// 		size: size,
-	// 		resolve: {
-	// 			items: function () {
-	// 				return $scope.items;
-	// 			}
-	// 		}
-	// 	});
-
-	// 	modalInstance.result.then(function (selectedItem) {
-	// 		$scope.selected = selectedItem;
-	// 	}, function () {
-	// 		console.log("modal dismissed at: " + new Date());
-	// 	});
-	// };
-
-	// self.isOwner = function(game) {
-	// 	if (game.createdBy._id === window.localStorage.getItem("username")) {
-	// 		return true;
-	// 	} else {
-	// 		return false;
-	// 	}
-	// }
-
-	// self.join = function(gameId) {
-	// 	// $http.post("http://mahjongmayhem.herokuapp.com/games/" + gameId + "/players")
-	// 	// .success(function (response) {
-	// 	// 	console.log("join game response: " + response);
-	// 		// window.location.href="/";
-
-	// 	// })
-	// 	// .error(function (response, status) {
-	// 	// 	console.log("join game error response: " + response);
-	// 	// 	console.log("join game error status: " + status);
-
-	// 	// 	if (status == 400) {
-	// 	// 		if (response.message == "") {
-	// 	// 			alert("cant join a game twice");
-	// 	// 		} else {
-	// 	// 			alert("You have to be logged in");
-	// 	// 		}
-	// 	// 	}
-	// 	// })
-	// };
-
-	// self.startGame = function(gameId) {
-	// 	// $http.post("http://mahjongmayhem.herokuapp.com/games/" + gameId + "/start")
-	// 	// .success(function (response) {
-	// 	// 	console.log("start game response: " + response);
-	// 	// 	// window.location.href="/";
-	// 	// });
-	// };
-
+	
 	function init() {
 		getGames();
 		getGameTemplates();
