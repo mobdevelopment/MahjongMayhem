7400156fb2eb8aaf749dae58e6c3cddcc5f38fc9
Michael Schouten <manschouten@hotmail.com>
2017-04-11 09:24:09 +0200

    theme not working styling in progress

--- app/js/general/controllers/generalController.js
+++ app/js/general/controllers/generalController.js
@@ -3,4 +3,14 @@ module.exports = function($scope, authService) {
 	// $scope.token = window.localStorage['token'];
 	self.currentUser = authService.getUser();
 
+	// if(window.localStorage['theme'] != 'undefined')
+	// {
+	// 	$scope.myDynamicClass = window.localStorage['theme'];
+	// }
+
+	// self.theme = function(theme) {
+	// 	$scope.myDynamicClass = theme;
+	// 	window.localStorage['theme'] = theme;
+	// };
+
 };
